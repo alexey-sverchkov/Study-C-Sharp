e80f8c712c196fec1e42809c02c7a7d2b526aad1
Alexey Sverchkov <alexey.sverchkov@mail.ru>
2020-10-02 23:25:34 +0300

    finish task 1.5 add function to print all words that contains given substring

---
+++ C-Sharp/Task1_5/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter string");
+            String message = Console.ReadLine();
+            Console.WriteLine("Please enter substring");
+            String substr = Console.ReadLine();
+            PrintWordsWithGivenSubstring(message, substr);
+        }
+
+        static void PrintWordsWithGivenSubstring(String message, String substr)
+        {
+            String pattern = @"\W+";
+            String[] words = System.Text.RegularExpressions.Regex.Split(message, pattern);
+            Console.WriteLine("Words which contains substring: {0}", substr);
+            foreach(String word in words){
+                if (word.Contains(substr))
+                {
+                    Console.Write(word + "\t");
+                }
+            }
+        }
+    }
+}
