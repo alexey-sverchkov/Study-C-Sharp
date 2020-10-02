255db42f41e2a3f0ce45226d07ad3b7b6283a4d1
Alexey Sverchkov <alexey.sverchkov@mail.ru>
2020-10-02 23:04:02 +0300

    finish task 1.4 add function that appends symbol x after each occurence of symbol y in given string

---
+++ C-Sharp/Task1_4/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter string");
+            String str = Console.ReadLine();
+            String replacedStr = AppendSymbolAfterSymbol('x', 'y', str);
+            Console.WriteLine("Modified string:\n{0}", replacedStr);
+
+        }
+
+        // appends symbol x after each occurence of symbol y in string
+        static String AppendSymbolAfterSymbol(char x, char y, String str)
+        {
+            StringBuilder sb = new StringBuilder(str);
+            String strToReplace = y.ToString() + x.ToString();
+            sb.Replace(y.ToString(), strToReplace);
+            return sb.ToString();
+        }
+    }
+}
