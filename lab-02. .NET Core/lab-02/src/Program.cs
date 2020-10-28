3e128165d44489fa5901d2826bb78006e83d3b64
Alexey Sverchkov <alexey.sverchkov@mail.ru>
2020-10-28 21:05:47 +0300

    [Lab02] add parsing user from appsettings.json add check if user login or password differ from cli arguments for user

--- lab-02. .NET Core/lab-02/src/Program.cs
+++ lab-02. .NET Core/lab-02/src/Program.cs
@@ -1,32 +1,56 @@
 ﻿using CommandLine;
 using lab_02.src;
+using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 
 namespace lab_02
 {
     class Program
     {
         static void Main(string[] args)
-        {                       
-            CommandLine.Parser.Default.ParseArguments<CommandLineOptions>(args)
-                .WithParsed(RunOptions)
-                .WithNotParsed(HandleParseError);            
-        }
-
-        static void RunOptions(CommandLineOptions opts)
         {
-            String username = opts.Username;
-            String password = opts.Password;
+            // get user login and password from cli
+            var startOptions = CommandLineParser.ParseCommandLineArgs(args); 
+            
+            // invalid number of parameters
+            if (startOptions == null)
+            {
+                return;
+            }
 
-            Console.WriteLine($"username: {username}");
-            Console.WriteLine($"password: {password}");
-        }
+            String parsedUsername = startOptions.Username;
+            String parsedPassword = startOptions.Password;
 
-        static void HandleParseError(IEnumerable<Error> errors)
-        {   
-            // basic handling errors 
-        }
+
+            // get user login and password from appsettings json file
+            IConfiguration config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", true, true)
+                .Build();
+
+            String configUsername = config["User:Login"];
+            String configPassword = config["User:Password"];
+        
+
+            // input login of user is unknown
+            if (!parsedUsername.Equals(configUsername))
+            {
+                Console.WriteLine($"Error: User {parsedUsername} is not found!");
+                return;
+            }
+
+            if (!parsedPassword.Equals(configPassword))
+            {
+                Console.WriteLine($"Error: Input password of user: {parsedUsername} is not correct!");
+                return;
+            }
+
+            Console.WriteLine("Successfully logged in!");
+
+           
+        }                
     }
 }
