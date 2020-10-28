873fdb05f22e72cb3c2ea375bbc48261eedda0d0
Alexey Sverchkov <alexey.sverchkov@mail.ru>
2020-10-28 13:10:08 +0300

    add parsing cli parameters use package CommandLineParser

---
+++ lab-02. .NET Core/lab-02/src/CommandLineOptions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CommandLine;
+
+namespace lab_02.src
+{
+    public sealed class CommandLineOptions
+    {
+        [Option('l', "login", Required = true, HelpText = "Login of user")]
+        public String Username { get; set; }
+
+        [Option('p', "password", Required = true, HelpText = "Password of user")]
+        public String Password { get; set; }
+    }
+}
---
+++ lab-02. .NET Core/lab-02/src/Program.cs
@@ -0,0 +1,32 @@
+﻿using CommandLine;
+using lab_02.src;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace lab_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {                       
+            CommandLine.Parser.Default.ParseArguments<CommandLineOptions>(args)
+                .WithParsed(RunOptions)
+                .WithNotParsed(HandleParseError);            
+        }
+
+        static void RunOptions(CommandLineOptions opts)
+        {
+            String username = opts.Username;
+            String password = opts.Password;
+
+            Console.WriteLine($"username: {username}");
+            Console.WriteLine($"password: {password}");
+        }
+
+        static void HandleParseError(IEnumerable<Error> errors)
+        {   
+            // basic handling errors 
+        }
+    }
+}
