c79e82367029a37e5da4bde5b35358926d60c34d
Alexey Sverchkov <alexey.sverchkov@mail.ru>
2020-10-28 21:04:34 +0300

    [Lab02] add class for cli arguments parsing

---
+++ lab-02. .NET Core/lab-02/src/CommandLineParsing/CommandLineParser.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CommandLine;
+
+namespace lab_02.src
+{
+    public static class CommandLineParser
+    {
+        public static CommandLineOptions ParseCommandLineArgs(this string[] args)
+        {
+            var options = new CommandLineOptions();
+            var hasCommandLineErrors = false;
+
+            CommandLine.Parser.Default.ParseArguments<CommandLineOptions>(args)
+                .WithParsed(x =>
+                {
+                    options.Username = x.Username;
+                    options.Password = x.Password;
+                })
+                .WithNotParsed(_ => hasCommandLineErrors = true);
+
+            if (hasCommandLineErrors)
+            {
+                return null;
+            }
+
+            return options;
+        }
+    }
+}
