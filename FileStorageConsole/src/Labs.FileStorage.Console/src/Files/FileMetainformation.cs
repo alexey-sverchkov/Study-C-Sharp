752a6edb58069164cd3ed97f864f31042959f694
Alexey Sverchkov <alexey.sverchkov@mail.ru>
2020-11-05 20:34:37 +0300

    [Lab02] add class FileMetainformation to store metainformation of file

---
+++ FileStorageConsole/src/Labs.FileStorage.Console/src/Files/FileMetainformation.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lab_02.src.Files
+{
+    public class FileMetainformation
+    {
+        // properties with fields
+        public  String   Name { get; set; } // file name
+        public  String   Extension { get; set; }
+
+        private ulong    size;
+        public  ulong    Size {
+            get
+            {
+                return size;
+            }
+            set
+            {
+                if (value >= 0)
+                {
+                    size = value;
+                }
+                else
+                {
+                    Console.WriteLine("Warning, size of file can't be less than zero");
+                    size = 0;
+                }
+            }
+        }
+        public  DateTime CreationDate { get; set; }
+
+        private uint     downloadsNumber = 0;
+        public  uint     DownloadsNumber
+        {
+            get
+            {
+                return downloadsNumber;
+            }
+            set
+            {
+                if (value >= 0)
+                {
+                    downloadsNumber = value;
+                }
+                else
+                {
+                    Console.WriteLine("Warning, number of downloads of file can't be less than zero");
+                    downloadsNumber = 0;
+                }
+            }
+        }
+
+        // constructors
+        public FileMetainformation(String name, String extension, ulong size, DateTime creationDate)
+        {
+            Name = name;
+            Extension = extension;
+            Size = size;
+            CreationDate = creationDate;
+        }
+
+
+        // Methods
+        public override String ToString()
+        {
+            return $"FileMetainformation [ name: {Name}, " +
+                                         $"extension: {Extension}, " +
+                                         $"size: {Size}, " + $"creation date: {CreationDate.ToString("F")}" +
+                                         $"number of downloads: {DownloadsNumber}";
+        }
+    }
+}
