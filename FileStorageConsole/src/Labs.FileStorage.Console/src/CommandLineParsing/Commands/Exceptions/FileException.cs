95123013b3321e6ed8a3e1bfdb9c483b02dec787
Alexey Sverchkov <alexey.sverchkov@mail.ru>
2020-11-14 22:48:34 +0300

    [Lab02] add FileException class

---
+++ FileStorageConsole/src/Labs.FileStorage.Console/src/CommandLineParsing/Commands/Exceptions/FileException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Labs.FileStorage.Console.CommandLineParsing.Commands.Exceptions
+{
+    [Serializable]
+    public class FileException : Exception
+    {
+        public FileException() 
+        { }
+
+        public FileException(String message)
+            : base(message)
+        { }
+
+        public FileException(String message, Exception innerException)
+            : base(message, innerException)
+        { }
+    }
+}
