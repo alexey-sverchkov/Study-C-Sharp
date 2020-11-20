30eaf8c8a7c3f2e338f797691e79c695f88af002
Alexey Sverchkov <alexey.sverchkov@mail.ru>
2020-11-20 17:08:02 +0300

    [Lab02] refactor ExportFormat class

--- FileStorageConsole/src/Labs.FileStorage.Console/src/Files/Export/ExportFormat.cs
+++ FileStorageConsole/src/Labs.FileStorage.Console/src/Files/Export/ExportFormat.cs
@@ -1,9 +1,31 @@
-﻿namespace Labs.FileStorage.Console.Files.Export
+﻿using System;
+using System.Collections.Generic;
+
+namespace Labs.FileStorage.Console.Files.Export
 {
-    public enum ExportFormat
+    public sealed class ExportFormat
     {
-        NoSpecified = 0, // to make sure that we not miss this field to assign
-        Json = 1,
-        XML = 2
-    }
+        public static readonly ExportFormat Undefined = new ExportFormat("undefined"); // to make sure that we not miss this field to assign
+        public static readonly ExportFormat Json = new ExportFormat("json");
+        public static readonly ExportFormat XML = new ExportFormat("xml");
+
+        private static readonly SortedList<String, ExportFormat> Values = new SortedList<string, ExportFormat>();
+        private        readonly String Value;
+
+        private ExportFormat(String value)
+        {
+            Value = value;
+            Values.Add(value, this);
+        }
+
+        public static implicit operator ExportFormat(String value) 
+        {
+            return Values[value];
+        }
+
+        public static implicit operator String(ExportFormat value)
+        {
+            return value.Value;
+        }
+    }    
 }
