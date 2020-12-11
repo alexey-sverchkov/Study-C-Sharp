0ec551700f53ee0ad5c9ff8427372aaf497f9782
Alexey Sverchkov <alexey.sverchkov@mail.ru>
2020-12-11 23:06:19 +0300

    [Lab03] cosmetic fixes

--- FileStorageConsole/src/Labs.FileStorage.Console/src/ApplicationContext.cs
+++ FileStorageConsole/src/Labs.FileStorage.Console/src/ApplicationContext.cs
@@ -6,8 +6,8 @@ namespace Labs.FileStorage.Console
     public static class ApplicationContext
     {
         public static Domain.Users.User               User { get; set; }
-        public static Data.Files.FileStorage               FileStorage { get; set; }
-        public static Data.Files.Database                  Database { get; set; }
+        public static Data.Files.FileStorage          FileStorage { get; set; }
+        public static Data.Files.Database             Database { get; set; }
         public static List<MetainformationExporter>   MetainformationExporters { get; set; }
     }
 }
