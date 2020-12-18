b92d89a3b43967f6509661a65aace219be85f352
Alexey Sverchkov <alexey.sverchkov@mail.ru>
2020-12-19 01:03:11 +0300

    [Lab04] add task 4.2

--- SQL/Labs.SQL/Scripts/task4, changing_tables, creation_of_PK and Constraints, INSERT operator.sql
+++ SQL/Labs.SQL/Scripts/task4, changing_tables, creation_of_PK and Constraints, INSERT operator.sql
@@ -45,3 +45,58 @@ FROM dbo.Person;
 -- 6.
 ALTER TABLE dbo.Person
 ALTER COLUMN Title NVARCHAR(6);
+
+
+--- TASK 4.2
+
+-- 1.
+CREATE TABLE dbo.StateProvince(
+	StateProvinceID			INT,
+	StateProvinceCode		NCHAR(3),
+	CountryRegionCode		NVARCHAR(3),
+	IsOnlyStateProvinceFlag BIT,
+	Name					NVARCHAR(50),
+	TerritoryID				INT,
+	ModifiedDate			DATETIME
+);
+
+-- 2.
+ALTER TABLE dbo.StateProvince
+ADD UNIQUE(Name);
+
+-- 3.
+ALTER TABLE dbo.StateProvince
+ADD CONSTRAINT noDigits_CountryRegionCode
+CHECK (CountryRegionCode LIKE '[^0-9]%');
+
+-- 4.
+ALTER TABLE dbo.StateProvince
+ADD CONSTRAINT df_ModifiedDate
+DEFAULT(GETDATE()) FOR ModifiedDate;
+
+-- 5.
+INSERT INTO dbo.StateProvince
+SELECT sp.StateProvinceID, sp.StateProvinceCode, sp.CountryRegionCode, sp.IsOnlyStateProvinceFlag,
+		sp.Name, sp.TerritoryID, sp.ModifiedDate
+FROM AdventureWorks2017.Person.StateProvince AS sp
+INNER JOIN AdventureWorks2017.Person.CountryRegion AS cp
+ON (sp.Name = cp.Name);
+
+-- print inserted values
+SELECT *
+FROM dbo.StateProvince;
+
+-- 6.
+ALTER TABLE dbo.StateProvince
+DROP COLUMN IsOnlyStateProvinceFlag;
+
+-- print table after removing field 'IsOnlyStateProvinceFlag'
+SELECT *
+FROM dbo.StateProvince;
+
+ALTER TABLE dbo.StateProvince
+ADD Population INT;
+
+-- print table after creation of field 'Population', which supports NULL values
+SELECT *
+FROM dbo.StateProvince;
